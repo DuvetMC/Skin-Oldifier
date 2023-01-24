2b41dade81d521bbd85c1588f8a5af011d6dc1e0
Octal <59418903+oliver-makes-code@users.noreply.github.com>
2023-01-24 00:08:51 -0600

    Display hat over head

--- head_only.ts
+++ head_only.ts
@@ -4,7 +4,9 @@ export default function getHead(image: Image) {
     const out = new Image(8, 8)
     for (let x = 0; x < 8; x++) {
         for (let y = 0; y < 8; y++) {
-            out.setPixelAt(x+1, y+1, image.getPixelAt(x+9, y+9))
+            const head = image.getPixelAt(x+9, y+9)
+            const hat = image.getPixelAt(x+41, y+9)
+            out.setPixelAt(x+1, y+1, hat == 0 ? head : hat)
         }
     }
     return out.resize(64,64)
